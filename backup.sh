5b8f5f381dbd4131ac0f0a4e33c713040ae889b1
chirayupatil <chirayupatil@hkbdevelopment.com>
2024-11-03 09:49:37 +0530

    Backup dir write permission check added

--- backup.sh
+++ backup.sh
@@ -3,6 +3,12 @@
 backup_dir="/tmp/"
 source_dir="/home/chirayupatilubuntu/trial/my_project/"
 
+#Check for Permission Issue
+if [ ! -w "$backup_dir" ]; then
+    echo "Error: No write permission for the backup directory: $backup_dir"
+    exit 1
+fi
+
 #Create a timestamped backup of the source directory
 tar -czf "$backup_dir/backup_$(date +%Y%m%d_%H%M%S).tar.gz" -C "$source_dir" .
 
